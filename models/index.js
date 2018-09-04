16eed2a166c27fc1dd70ff23181d9ebaab080722
Catherine Chan <learnafew@gmail.com>
2018-09-03 18:05:55 -0700

    Fixed GET route to return all data from the database

--- models/index.js
+++ models/index.js
@@ -19,11 +19,10 @@ if (config.use_env_variable) {
   );
 }
 
-fs.readdirSync(__dirname)
+fs
+  .readdirSync(__dirname)
   .filter(function(file) {
-    return (
-      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
-    );
+    return (file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js");
   })
   .forEach(function(file) {
     var model = sequelize.import(path.join(__dirname, file));
