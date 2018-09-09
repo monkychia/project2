5abbb6847bd475b36dfedf24a88c702c8d815359
Catherine Chan <learnafew@gmail.com>
2018-09-09 15:20:21 -0700

    Added email support when user clicks Submit in create page

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 var db = require("../models");
+const nodemailer = require('nodemailer');
 
 module.exports = function (app) {
 
@@ -20,6 +21,40 @@ module.exports = function (app) {
       glutenFree: req.body.glutenFree
     })
       .then(function (results) {
+
+        // console.log('----- i am in post', res);
+
+        // nodemailer.createTestAccount((err, account) => {
+        //   let transporter = nodemailer.createTransport({
+        //       host: 'smtp.gmail.com',
+        //       port: 587,
+        //       secure: false, 
+        //       auth: {
+        //         user: "projecttwo22@gmail.com", // sender's credentials
+        //         pass: "pr0ject2!"// sender's password
+        //       }
+        //   });
+
+        //   // setup email data with unicode symbols
+        //   let mailOptions = {
+        //       from: '"test <projecttwo22@gmail.com>', // sender address
+        //       to: 'learnafew@gmail.com', // list of receivers
+        //       subject: 'Hello ✔', 
+        //       text: 'Hello world?', // plain text body
+        //       html: '<b>Hello world?</b>' // html body
+        //   };
+
+        //   // send mail with defined transport object
+        //   transporter.sendMail(mailOptions, (error, info) => {
+        //       if (error) {
+        //           return console.log(error);
+        //       }
+        //       console.log('Message sent: %s', info.messageId);
+        //       console.log(info);
+        //       console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        //   });
+        // });
+
         res.json(results);
       });
   });
@@ -56,6 +91,7 @@ module.exports = function (app) {
   app.get("/api/eventList/", function (req, res) {
     db.EventList.findAll({})
       .then(function (results) {
+
         res.json(results);
       });
   });
@@ -70,6 +106,47 @@ module.exports = function (app) {
       additionalInfo: req.body.additionalInfo
     })
       .then(function (results) {
+
+        let body = `<div>Hello Manager,</div>
+                    <div><p>We received a new event request with the followings:</p><hr/>
+                      <div>
+                        <p>Event Name: ${req.body.eventName}</p>
+                        <p>Contact Person: ${req.body.contactName}</p>
+                        <p>Event Date: ${req.body.eventDate}</p>
+                        <p>Description: ${req.body.description}</p>
+                        <p>Orders: ${req.body.additioanlInfo}</p>
+                      </div>
+                    </div>`;
+
+        nodemailer.createTestAccount((err, account) => {
+          let transporter = nodemailer.createTransport({
+              host: 'smtp.gmail.com',
+              port: 587,
+              secure: false, 
+              auth: {
+                user: "projecttwo22@gmail.com", // sender's credentials
+                pass: "pr0ject2!"// sender's password
+              }
+          });
+
+          // setup email data with unicode symbols
+          let mailOptions = {
+              from: '"CaterApp Admin <projecttwo22@gmail.com>', // sender address
+              to: 'learnafew@gmail.com', // list of receivers
+              subject: `Hello ✔`, 
+              text: `Hello world?`, // plain text body
+              html: body // html body
+          };
+
+          // send mail with defined transport object
+          transporter.sendMail(mailOptions, (error, info) => {
+              if (error) {
+                  return console.log(error);
+              }
+              console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+          });
+        });
+
         res.json(results);
       });
   });
